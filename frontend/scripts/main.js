09e6a3eec73484b8f53c4adf406b36017d402836
wawanneutron <wawanneutron1331@gmail.com>
2020-10-01 19:13:35 +0700

    make navbar colapse using javaScript

--- frontend/scripts/main.js
+++ frontend/scripts/main.js
@@ -1,4 +1,5 @@
-//ketika di scroll muncul icon scroll ke atas
+// make projact portfolio Neutron Blog
+// when in the scroll, appears icon scroll to top
 $(window).scroll(function () {
     var totalHeight = $(window).scrollTop();
     if (totalHeight > 350) {
@@ -8,9 +9,28 @@ $(window).scroll(function () {
     }
 });
 
-//proses scroll
+//process scroll
 $('a.scrolltotop').on('click', function (event) {
     $([document.documentElement, document.body]).animate({
         scrollTop: $("#home").offset().top
     }, 300);
 });
+
+// make navbar collapse when in scroll to down lost
+(function () {
+    var documentElem = $(document),
+        nav = $('.navbar-custom'),
+        lastScrollTop = 0;
+
+    documentElem.on('scroll', function () {
+        var currentScrollTop = $(this).scrollTop();
+
+        // scroll down
+        if (currentScrollTop > lastScrollTop) nav.addClass('hidden');
+
+        // scrollTop
+        else nav.removeClass('hidden');
+        lastScrollTop = currentScrollTop;
+    });
+
+})();
